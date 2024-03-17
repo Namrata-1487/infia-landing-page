55c4660a3df32fe109a1745f08e07d68a8a562b9
namrata shirsat <namrata.shirsaat@gmail.com>
2024-03-17 22:41:51 +0530

    javascript added

---
+++ index.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll(".count");
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const c = +counter.innerText;
+
+    const increment = target / 100;
+    console.log(increment);
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`;
+      setTimeout(updateCounter, 100);
+    } else {
+      counter.innerText = target;
+    }
+  };
+
+  updateCounter();
+});
