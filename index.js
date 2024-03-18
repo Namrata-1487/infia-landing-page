759f287fa8933896cd34a2de77d5b6366d9af8f4
namrata shirsat <namrata.shirsaat@gmail.com>
2024-03-18 14:53:36 +0530

    latest final

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ counters.forEach((counter) => {
 
     if (c < target) {
       counter.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 15);
+      setTimeout(updateCounter,25);
     } else {
       counter.innerText = target;
     }
