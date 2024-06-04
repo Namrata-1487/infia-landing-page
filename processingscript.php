3a9238068d0807c0ff06730d674f2542f4d8e2cf
namrata shirsat <namrata.shirsaat@gmail.com>
2024-06-04 17:06:55 +0530

    php updated

---
+++ processingscript.php
@@ -0,0 +1,12 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    // retrive from data 
+    $fname = $_POST["fname"] ;
+    $lname = $_POST["lname"] ;
+   
+    // display data
+    echo "First Name: ". $fname ." <br> ";
+    echo "Last Name: " . $fname ." <br> ";
+    
+}
+?>
