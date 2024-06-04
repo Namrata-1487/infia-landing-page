a94807423302fc7c69aad155445935738b31671f
namrata shirsat <namrata.shirsaat@gmail.com>
2024-06-04 16:18:11 +0530

    form edited

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 want to run the project kindly click on hosted link https://infia-landing-page.netlify.app
 or download the file in VS code and run it 
+to open form kindly click on contact us page 
--- action_page.php
+++
@@ -1,9 +0,0 @@
-<html>
-
-<body>
-    User first name <?php exho$_POST["fname"]; ?> <br>
-    User Lsat name <?php exho$_POST["lname"]; ?> <br>
-    Enquire about<?php exho$_POST["query"]; ?> <br>
-</body>
-
-</html>
